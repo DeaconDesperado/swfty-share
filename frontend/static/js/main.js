c937d38b250e5b4dca1c6590bc88fdfac070279b
Mark Grey <mark@trevbuntu.(none)>
2012-07-23 16:57:43 -0400

    got images working

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -15,7 +15,16 @@ $(function(){
         $.each(d['stories'],function(ind){
             story = d['stories'][ind];
         });
-        var line = '<li>Uploader: '+story.uploader+'</li>';
+        var line = '<li>Uploader: '+story.uploader;
+        line += 'Description: '+story.description;
+        $.each(story['file_data'],function(ind){
+            file = story['file_data'][ind];
+            console.log(file)
+            if(file.mimetype == 'image/jpeg' || file.mimetype == 'image/png'){
+                line += '<img class="story_image" src="/image/'+file._id+'" />'
+            }
+        });
+        line += '</li>'
         markup += line;
         $story_list.html(markup);
     }
