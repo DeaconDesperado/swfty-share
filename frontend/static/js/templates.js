493e4a64022b3ae1590928f61f8134065d81d2a0
Mark Grey <mark@trevbuntu.(none)>
2012-07-24 10:47:31 -0400

    trying to do this with underscore templates

---
+++ frontend/static/js/templates.js
@@ -0,0 +1,7 @@
+var templates = {};
+
+templates.story_cell = ' \
+                        <li>\
+                        <span class="uploader"><%= uploader%></span> \
+                        </li>\
+                        ';
